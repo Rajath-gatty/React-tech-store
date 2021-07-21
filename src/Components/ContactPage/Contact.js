0df1bf6fb101fddec091172294aeea74e1bd8a3f
Rajath-gatty <rajathgatty253@gmail.com>
2021-07-21 19:36:40 +0530

    updated package.json

--- src/Components/ContactPage/Contact.js
+++ src/Components/ContactPage/Contact.js
@@ -2,20 +2,28 @@ import React from 'react'
 import Title from '../Title'
 
 const Contact = () => {
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     fetch('/api/contact-form',)
+    //     .then((response) => {
+    //         response.json();
+    //     })
+    //     .then(res => {
+    //         console.log(res);
+    //         window.location.reload();
+    //     })
+    // }
     return (
         <section className="py-5">
             <div className="row">
                 <div className="col-10 mx-auto col-md-6 my-3">
                     <Title title="contact us" />
-                    <form action="https://formspree.io/mgenybed" method="POST" className="mt-5" >
+                    <form action="/api/contact-form" method="POST" className="mt-5" >
                         <div className="form-group">
                             <input type="text" name="Name" className="form-control" placeholder="FirstName" />
                         </div>
                         <div className="form-group">
                             <input type="email" name="email" className="form-control" placeholder="email@email.com" />
-                        </div>
-                        <div className="form-group">
-                            <input type="text" name="subject" className="form-control" placeholder="important!!" />
                         </div>
                         <div className="form-group">
                             <textarea type="text" name="message" row="10" className="form-control" placeholder="Your message" />
